0902972a5aa3607a758f5d1a0d4d7b27b2e5cdbb
luvnft <140175751+luvnft@users.noreply.github.com>
2025-10-27 11:49:08 -0400

    Update product-list.tsx

--- src/pages/product-list.tsx
+++ src/pages/product-list.tsx
@@ -28,10 +28,10 @@ const ProductList: React.FC = () => {
 
       <div data-aos="fade-down">
         <Link to="/" className="mb-4 text-3xl font-bold text-white no-underline">
-          🚀 DELIV3RY 🌟
+          ⚡ @DELIV3RY
           <br />
           <span className="text-[15px] italic font-normal">
-            TikTok POD (Proof of Delivery) reel deliveries. 😎
+            😎 TikTok POD (Proof of Delivery) reel delivery campaigns.
           </span>
         </Link>
       </div>
